cd98b5351f1b3fe50feb08a13870daf96f99f6ed
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-02-07 07:36:08 +0200

    Init

---
+++ src/main/java/com/hladchenko/newyearnotifier/NewYearNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.hladchenko.newyearnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYearNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewYearNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hladchenko/newyearnotifier/NewYearNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hladchenko.newyearnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYearNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
